563db3d570b219754bbd40dced4c18db5ca73061
Mahmoud Mustafa <dardier138@gamil.com>
2025-07-22 14:19:06 +0300

    feat: refactor statistics hooks and services for improved data fetching and structure

--- src/pages/OrdersManagementPage.tsx
+++ src/pages/OrdersManagementPage.tsx
@@ -1,43 +1,65 @@
 import { StatCard } from "@/components/cards/StatCard";
+import StatCardSkeletons from "@/components/cards/StatCardSkeleton";
+import { ErrorFetchingData } from "@/components/ErrorFetchingData";
 import OrderTable from "@/components/tables/order-table/OrderTable";
 import Box from "@/components/ui/box";
+import useGetStats from "@/hooks/statistics/useGetGeneralStatistics";
 import { AlertCircle, CheckCircle, PlayCircle, RotateCcw } from "lucide-react";
 
 const OrdersManagementPage = () => {
+  const { data, isPending, error, refetch } =
+    useGetStats<IOrderStatusStatistic>("ordersStatus", "orders");
+
+  if (isPending) {
+    return (
+      <div className="grid lg:grid-cols-3 sm:grid-cols-2 flex-wrap gap-3 mt-3">
+        <StatCardSkeletons />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="grid bg-white rounded-2xl shadow-lg  grid-cols-1 gap-3 mt-3">
+        <ErrorFetchingData onRetry={() => refetch?.()} />
+      </div>
+    );
+  }
+
   return (
     <Box className="w-full ">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mb-6 gap-6">
         <StatCard
-          title="Completed"
-          value={16}
-          change="+8 هذا الشهر"
+          title={`${data?.data[0].status}`}
+          change={`${data?.data[0].newThisMonth} طلبات مكتملة جدد هذا الشهر`}
+          value={`${data?.data[0].totalCount}`}
           changeDirection="up"
           icon={<CheckCircle />}
-          color="green"
+          color="blue"
         />
         <StatCard
-          title="Failed"
-          value={16}
-          change="+8 هذا الشهر"
-          changeDirection="down"
-          icon={<AlertCircle />}
-          color="pink"
-        />
-        <StatCard
-          title="In progress"
-          value={25}
-          change="+8 هذا الشهر"
+          title={`${data?.data[1].status}`}
+          value={`${data?.data[1].totalCount}`}
+          change={`${data?.data[1].newThisMonth} طلبات قيد التنفيذ جدد هذا الشهر`}
           changeDirection="up"
-          icon={<PlayCircle />}
+          icon={<AlertCircle />}
           color="yellow"
         />
         <StatCard
-          title="Active"
-          value={5}
-          change="+5 هذا الشهر"
+           title={`${data?.data[2].status}`}
+          value={`${data?.data[2].totalCount}`}
+          change={`${data?.data[2].newThisMonth} طلبات  مكتملة جدد هذا الشهر`}
+          changeDirection="up"
+          icon={<PlayCircle />}
+          color="green"
+        />
+        <StatCard
+              title={`${data?.data[3].status}`}
+          value={`${data?.data[3].totalCount}`}
+          change={`${data?.data[3].newThisMonth} طلبات  فاشله جدد هذا الشهر`}
           changeDirection="up"
           icon={<RotateCcw />}
-          color="blue"
+          color="pink"
         />
       </div>
       <OrderTable />
--- src/pages/UsersManagementPage.tsx
+++ src/pages/UsersManagementPage.tsx
@@ -2,35 +2,59 @@ import { StatCard } from "@/components/cards/StatCard";
 import UsersTable from "@/components/tables/users-table/UsersTable";
 import Box from "@/components/ui/box";
 import { BriefcaseBusiness, Container, Handshake } from "lucide-react";
+import StatCardSkeletons from "@/components/cards/StatCardSkeleton";
+import { ErrorFetchingData } from "@/components/ErrorFetchingData";
+import useGetStats from "@/hooks/statistics/useGetGeneralStatistics";
 
 export default function UsersManagementPage() {
+  const { data, isPending, error, refetch } = useGetStats<IUserStatistics>(
+    "usersStatus",
+    "users"
+  );
+
+  if (isPending) {
+    return (
+      <div className="grid lg:grid-cols-3 sm:grid-cols-2 flex-wrap gap-3 mt-3">
+        <StatCardSkeletons />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="grid bg-white rounded-2xl shadow-lg  grid-cols-1 gap-3 mt-3">
+        <ErrorFetchingData onRetry={() => refetch?.()} />
+      </div>
+    );
+  }
+
   return (
     <Box className="w-full">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mb-6 gap-6">
         <StatCard
-          title="عدد المستوردين"
-          value="32 218"
-          change="5.38% فتره التغيير"
-          changeDirection="down"
-          color="blue"
+          title={`${data?.data[0].category}`}
+          change={`${data?.data[0].newThisMonth} طلبات مكتملة جدد هذا الشهر`}
+          value={`${data?.data[0].totalCount}`}
+          changeDirection="up"
+          color="pink"
           icon={<Handshake className="w-6 h-6 cursor-pointer" />}
         />
 
         <StatCard
-          title="عدد الموردين"
-          value={218}
-          change="3.59% فتره التغيير"
+          title={`${data?.data[1].category}`}
+          change={`${data?.data[1].newThisMonth} طلبات مكتملة جدد هذا الشهر`}
+          value={`${data?.data[1].totalCount}`}
           changeDirection="up"
           color="green"
           icon={<Container className="w-6 h-6 cursor-pointer" />}
         />
 
         <StatCard
-          title="عدد الباحثين عن عمل"
-          value={298}
-          change="6.84% فتره التغيير"
+          title={`${data?.data[2].category}`}
+          change={`${data?.data[2].newThisMonth} طلبات مكتملة جدد هذا الشهر`}
+          value={`${data?.data[2].totalCount}`}
           changeDirection="up"
-          color="purple"
+          color="yellow"
           icon={<BriefcaseBusiness className="w-6 h-6 cursor-pointer" />}
         />
       </div>
