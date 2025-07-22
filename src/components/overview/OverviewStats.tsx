c3d765dda59d4b7dc48cc84ef1bb9ae4a5ac8ed6
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-07-22 10:52:49 +0300

    feat: update OverviewStats to display dynamic user, order, and category counts

--- src/components/overview/OverviewStats.tsx
+++ src/components/overview/OverviewStats.tsx
@@ -1,7 +1,6 @@
 import { ChartBarStacked, ShoppingCart, Users } from "lucide-react";
 import { StatCard } from "../cards/StatCard";
 
-export default function OverviewStats()
 import useGetGeneralStatistics from "@/hooks/statistics/useGetGeneralStatistics";
 import { ErrorFetchingData } from "../ErrorFetchingData";
 import StatCardSkeletons from "../cards/StatCardSkeleton";
@@ -24,27 +23,27 @@ export default function OverviewStats() {
       </div>
     );
   }
-  
+
   return (
     <div className="grid lg:grid-cols-3 sm:grid-cols-2  flex-wrap gap-3 mt-3">
       <StatCard
         color="indigo"
-        change="+8 هذا الشهر"
-        value="20"
+        change={`${data?.data[0].newUsersThisMonth} مستخدمين جدد هذا الشهر`}
+        value={`${data?.data[0].totalUsers}`}
         title="كل المستخدمين"
         icon={<Users />}
       />
       <StatCard
         color="pink"
-        change="+8 هذا الشهر"
-        value="20"
+        change={`${data?.data[0].newOrdersThisMonth} طلبات جديدة هذا الشهر`}
+        value={`${data?.data[0].totalOrders}`}
         title="كل الطلبات"
         icon={<ShoppingCart />}
       />
       <StatCard
         color="green"
-        change="+8 هذا الشهر"
-        value="20"
+        change={`${data?.data[0].newCategoriesThisMonth} فئات جديدة هذا الشهر`}
+        value={`${data?.data[0].totalCategories}`}
         title="فئات"
         icon={<ChartBarStacked />}
       />
