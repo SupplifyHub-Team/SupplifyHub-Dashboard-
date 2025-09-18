b6f5e78e274e95aa497de75a2c633dfa78eaeed8
Mahmoud Alaa <mahmoud.alaa.dev1@gmail.com>
2025-09-18 10:32:10 +0300

    fix: remove unnecessary semicolon in BlogsManagement component

--- src/pages/BlogsManagement.tsx
+++ src/pages/BlogsManagement.tsx
@@ -3,7 +3,7 @@ import BlogsTable from "@/components/tables/blogs/BlogsTable";
 export default function BlogsManagement() {
   return (
     <div className="p-4 bg-accent-foreground rounded-lg">
-      <BlogsTable />;
+      <BlogsTable />
     </div>
   );
 }
